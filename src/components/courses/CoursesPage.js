d1d1f4e6fa6657a802fda127a9e1579299a12344
IvanLopezMedina <ivan.lopez.medina.ilm@gmail.com>
2020-04-01 18:00:57 +0200

    add/edit courses

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,40 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {connect} from 'react-redux';
-import * as courseActions from '../../redux/actions/courseActions';
-import * as authorActions from '../../redux/actions/authorActions';
+import {loadCourses} from '../../redux/actions/courseActions';
+import {loadAuthors} from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
 import CourseList from './CourseList';
+import {Redirect} from 'react-router-dom';
 
-class CoursesPage extends React.Component {
-  componentDidMount() {
-    const {courses, authors} = this.props;
+const CoursesPage = ({courses, authors, loadCourses, loadAuthors}) => {
+  const [redirectToAddCoursePage, setRedirectToAddCoursePage] = useState(false);
+
+  useEffect(() => {
     if (courses.length === 0) {
-      this.props.loadCourses().catch(error => {
+      loadCourses().catch(error => {
         alert('Loading courses failed' + error);
       });
     }
 
     if (authors.length === 0) {
-      this.props.loadAuthors().catch(error => {
+      loadAuthors().catch(error => {
         alert('Loading authors failed' + error);
       });
     }
-  }
+  });
 
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-        <CourseList courses={this.props.courses} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {redirectToAddCoursePage && <Redirect to="/course/" />}
+      <h2>Courses</h2>
+      <button
+        style={{marginBottom: 20}}
+        className="btn btn-primary add-course"
+        onClick={() => setRedirectToAddCoursePage(true)}
+      >
+        Add Course
+      </button>
+      <CourseList courses={courses} />
+    </>
+  );
+};
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
   authors: PropTypes.array.isRequired,
-  createCourse: PropTypes.func.isRequired,
   loadCourses: PropTypes.func.isRequired,
   loadAuthors: PropTypes.func.isRequired,
 };
@@ -55,10 +62,9 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchToProps = dispatch => ({
-  createCourse: course => dispatch(courseActions.createCourse(course)),
-  loadCourses: courses => dispatch(courseActions.loadCourses(courses)),
-  loadAuthors: authors => dispatch(authorActions.loadAuthors(authors)),
-});
+const mapDispatchToProps = {
+  loadCourses,
+  loadAuthors,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
